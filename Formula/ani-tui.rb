655c5c13202bac9f9296bd05c80be308a35e7e11
silent9669 <phucchemistry69@gmail.com>
2026-01-18 15:00:46 +0700

    Update formula URL

--- Formula/ani-tui.rb
+++ Formula/ani-tui.rb
@@ -1,10 +1,10 @@
 class AniTui < Formula
   desc "Anime TUI with image previews and terminal streaming"
-  homepage "https://github.com/silent9669/ani-TUI-for-macOS"
-  url "https://github.com/silent9669/ani-TUI-for-macOS/archive/refs/heads/master.tar.gz"
+  homepage "https://github.com/silent9669/ani-tui"
+  url "https://github.com/silent9669/ani-tui/archive/refs/heads/master.tar.gz"
   version "1.0.0"
   license "GPL-3.0-or-later"
-  head "https://github.com/silent9669/ani-TUI-for-macOS.git", branch: "master"
+  head "https://github.com/silent9669/ani-tui.git", branch: "master"
 
   depends_on "bash"
   depends_on "curl"
@@ -15,18 +15,13 @@ class AniTui < Formula
 
   def install
     bin.install "macos/ani-tui" => "ani-tui"
-    
-    # Install ani-cli for streaming
     libexec.install "ani-tui/ani-cli"
   end
 
   def caveats
     <<~EOS
-      ani-tui has been installed!
-
       For image previews: brew install chafa
       For video playback: brew install mpv
-
       Usage: ani-tui
     EOS
   end
